liferay/liferay-theme-tasks

commit c0d1ebaa18226251fe83ad3fe2f87a3e9c70518b

Improve watch task for 6.2

lib/util.js
@@ -32,6 +32,8 @@ module.exports = {
 	},
 
 	getSrcPath: function(srcPath, config, validator) {
+		var originalSrcPath = srcPath;
+
 		if (_.isUndefined(config)) {
 			config = {};
 		}
@@ -50,6 +52,16 @@ module.exports = {
 			}
 
 			srcPath = changedFile.path;
+
+			if (this.isCssFile(changedFileName) && config.version && config.version == '6.2') {
+				if (this.isSassPartial(changedFile.path)) {
+					return originalSrcPath;
+				}
+
+				changedFileName = changedFileName.replace(/\.css/, '.scss');
+
+				srcPath = path.join(originalSrcPath, '..', changedFileName);
+			}
 		}
 
 		return srcPath;

tasks/build.js
@@ -157,7 +157,7 @@ module.exports = function(options) {
 
 		if (supportCompass) {
 			runSequence('build:rename-css-files', function() {
-				cssPreprocessor(path.join(cssBuild, '**/*.scss'), {
+				cssPreprocessor(themeUtil.getSrcPath(path.join(cssBuild, '**/*.scss'), getSrcPathConfig()), {
 						compass: true,
 						loadPath: config.loadPath
 					})
@@ -242,7 +242,8 @@ module.exports = function(options) {
 	function getSrcPathConfig() {
 		return {
 			changedFile: store.get('changedFile'),
-			deployed: store.get('deployed')
+			deployed: store.get('deployed'),
+			version: themeConfig.version
 		};
 	}
 };

tasks/watch.js
@@ -20,6 +20,8 @@ module.exports = function(options) {
 
 			if (!fullDeploy && store.get('deployed')) {
 				runSequence(
+					'build:clean',
+					'build:base',
 					'build:src',
 					'build:web-inf',
 					'build:themelets',

commit 062f3e6931911271672faa0575d23bb755e90dcc

Fix issue with watch task for 6.2 (caused by c0d1eba)

lib/util.js
@@ -58,7 +58,11 @@ module.exports = {
 					return originalSrcPath;
 				}
 
-				changedFileName = changedFileName.replace(/\.css/, '.scss');
+				var cssExtChanged = _.isUndefined(config.cssExtChanged) ? true : config.cssExtChanged;
+
+				if (cssExtChanged) {
+					changedFileName = changedFileName.replace(/\.css/, '.scss');
+				}
 
 				srcPath = path.join(originalSrcPath, '..', changedFileName);
 			}

tasks/build.js
@@ -125,7 +125,11 @@ module.exports = function(options) {
 	});
 
 	gulp.task('build:src', function() {
-		return gulp.src(themeUtil.getSrcPath(path.join(options.pathSrc, '**/*'), getSrcPathConfig()), {
+		var srcPathConfig = getSrcPathConfig();
+
+		srcPathConfig.cssExtChanged = false;
+
+		return gulp.src(themeUtil.getSrcPath(path.join(options.pathSrc, '**/*'), srcPathConfig), {
 				base: './src'
 			})
 			.pipe(gulp.dest(pathBuild));

tasks/deploy.js
@@ -61,9 +61,21 @@ module.exports = function(options) {
 		var base = pathBuild;
 		var srcPath = pathBuild + '/**/*' + extname;
 
+		var version = themeConfig.liferayTheme.version;
+
 		if (extname != '.css') {
 			base = 'src';
 			srcPath = path.relative(process.cwd(), changedFile.path);
+		}
+		else if (version == '6.2') {
+			var srcPathConfig = {
+				changedFile: store.get('changedFile'),
+				cssExtChanged: false,
+				deployed: store.get('deployed'),
+				version: version
+			};
+
+			srcPath = themeUtil.getSrcPath(srcPath, srcPathConfig);
 		}
 
 		var stream = gulp.src(srcPath, {